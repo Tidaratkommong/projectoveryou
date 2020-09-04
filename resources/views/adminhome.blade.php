7d3f6abbb3ab289ff8358b4806e371606d1d2d60
Tidaratkommong <kommong24@gmail.com>
2020-09-05 03:33:55 +0700

    Update adminhome.blade.php

--- resources/views/adminhome.blade.php
+++ resources/views/adminhome.blade.php
@@ -8,10 +8,10 @@
     
         <div class="list-group" aria-label="Basic example">
             <a class="list-group-item active mt3" style="width: 12rem;">ระบบการจัดการร้านค้า</a>
-
+            <a href="{{url('admin/index_user')}}" class="list-group-item" style="width: 12rem;">จัดการสมาชิก</a>
             <a href="#" class="list-group-item" style="width: 12rem;">จัดการประเภทสินค้า</a>
-            <a href="{{url('product/index')}}" class="list-group-item" style="width: 12rem;">จัดการสินค้า</a>
-            @csrf
+            <a href="{{url('product/index_product')}}" class="list-group-item" style="width: 12rem;">จัดการสินค้า</a>
+
             <a href="#" class="list-group-item" style="width: 12rem;">จัดการโปรโมชั่น</a>
             <a href="#" class="list-group-item" style="width: 12rem;">ยันยันการสั่งซื้อ</a>
             <a href="#" class="list-group-item" style="width: 12rem;">จัดการโปรโมชั่น</a>
