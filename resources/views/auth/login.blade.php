78846320c3d98e0786ef2369a425af63aadd6241
Tidaratkommong <kommong24@gmail.com>
2020-11-11 17:14:47 +0700

    login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -8,7 +8,7 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card text-center">
-                <div class="card-header bg-info">{{ __('เข้าสู่ระบบ') }}</div>
+                <div class="card-header text-dark"><h3>{{ __('เข้าสู่ระบบ') }}</h3></div>
 
                 <div class="card-body">
                     <form method="POST" action="{{ route('login') }}">
