6d6dc5dffab1716fc49381359f15e7a438b35cfe
Tidaratkommong <kommong24@gmail.com>
2020-11-12 21:23:20 +0700

    admin.index_user

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -37,7 +37,7 @@ class DashboardController extends Controller
             'address' => 'required',
         ]);
         User::create($request->all());
-        return redirect()->route('admin')
+        return redirect()->route('admin.index_user')
                         ->with('success', ' new user created successfully');
     }
 
