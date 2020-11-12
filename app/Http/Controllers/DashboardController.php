4f0f4e2efc884cf00c82ec0434c564cdbf13f4c5
Tidaratkommong <kommong24@gmail.com>
2020-11-12 21:35:43 +0700

    admin

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -37,7 +37,7 @@ class DashboardController extends Controller
             'address' => 'required',
         ]);
         User::create($request->all());
-        return view('admin.index_user')
+        return view('admin')
                         ->with('success', ' new user created successfully');
     }
 
