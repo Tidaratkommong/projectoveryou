7e10d7378f720b6c5f5738358fed4d227c5f87d6
Tidaratkommong <kommong24@gmail.com>
2020-11-12 19:23:45 +0700

    welcome

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -62,7 +62,7 @@
                                         <div class="discount_price text-dark text-center"> จำนวนสินค้า : {{$value->product_num }} <span>ตัว</span> </div>
                                         <div class="price text-dark text-center"> ราคา : {{ $value->product_price }}<span> บาท </span></div>
                                         <div class="price text-dark text-center"> รายละเอียด : {{ $value->product_detail }}</div> <br/>
-                                        <div class="text-center text-danger "><button class="btn btn-outline my-1 my-sm-0" type="submit" ><a  href="{{ route('login') }}" /> สั่งซื้อสินค้า </a></button></div>
+                                        <div class="text-center text-danger "><button class="btn btn-outline my-1 my-sm-0 text-danger" type="submit" ><a  class=" text-danger" href="{{ route('login') }}" /> สั่งซื้อสินค้า </a></button></div>
                                         <!--<div class="text-center"> <a class=" text-danger"  />เพิ่มไปยังรถเข็น</a></div>-->
                                     </div>
                              </div>
