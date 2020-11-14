dcfd3bc37aca12dcc999849f11312bff4526c96f
Tidaratkommong <kommong24@gmail.com>
2020-11-14 20:50:09 +0700

    admin.update

--- resources/views/admin/edit_user.blade.php
+++ resources/views/admin/edit_user.blade.php
@@ -1,10 +1,8 @@
 @extends('layouts.app')
 
-@section('content')
-
-
+@section('content')                    
 <div class="container">
-<br />
+    <br />
     <a type="submit" name="submit" class="btn btn-danger " value="BACK"  href="{{url('admin/index_user')}}" />BACK</a>
     @csrf
     <br />
@@ -12,9 +10,9 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header text-dark"><h4>{{ __('จัดการข้อมูล') }}</h4></div>
-                <div class="card-body">
 
+                <div class="card-header"><h4>{{ __('แก้ไขข้อมูล') }}</h4></div>
+                <div class="card-body">
                 <form action="{{route('admin.update',$users->id)}}" method="post">
                       @csrf
                      @method('PUT')
@@ -24,21 +22,32 @@
                             {{session('success')}}
                         </div>
                         @endif
-
                         <div class="form-group row">
                             <label for="name" class="col-md-4 col-form-label text-md-right">{{ __('ชื่อผู้ใช้งาน :') }}</label>
 
                             <div class="col-md-6">
-                                <input id="name" type="text" class="form-control" name="name" value="{{$users['name'] }}" autocomplete="name" autofocus>
+                                <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{$users['name'] }}" autocomplete="name" autofocus>
 
+                                @error('username')
+                                <span class="invalid-feedback" role="alert">
+                                    <strong>{{ $message }}</strong>
+                                </span>
+                                @enderror
                             </div>
                         </div>
                        
+                        
                         <div class="form-group row">
                             <label for="address" class="col-md-4 col-form-label text-md-right">{{ __('ที่อยู่ :') }}</label>
 
                             <div class="col-md-6">
-                                <textarea id="address" type="text" class="form-control" name="address"  rows="3" autocomplete="address" autofocus/>{{$users['address'] }}</textarea>      
+                                <textarea id="address" type="text" class="form-control  @error('address') is-invalid @enderror" name="address"  rows="3" autocomplete="address" autofocus/>{{$users['address'] }}</textarea>
+                               
+                                @error('address')
+                                <span class="invalid-feedback" role="alert">
+                                    <strong>{{ $message }}</strong>
+                                </span>
+                                @enderror
                             </div>
                         </div>
                         <div class="form-group row">
@@ -59,7 +68,7 @@
                             <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail :') }}</label>
 
                             <div class="col-md-6">
-                                <input id="email" type="email" class="form-control" name="email" value="{{$users['email'] }}" autocomplete="email">
+                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{$users['email'] }}" autocomplete="email">
 
                                 @error('email')
                                 <span class="invalid-feedback" role="alert">
@@ -71,7 +80,7 @@
                         <div class="form-group row mb-0">
                             <div class="col-md-6 offset-md-4">
                                 <button type="submit" class="btn btn-primary btn-sm">
-                                    {{ __('แก้ไขข้อมูล') }}
+                                    {{ __('อัพเดทโปรไฟล์') }}
                                 </button>
                                 
                             </div>
@@ -83,6 +92,10 @@
         </div>
     </div>
 </div>
+
+
+
+
 
 
 
