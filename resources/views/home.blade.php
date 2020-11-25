cd31d3b2f5b3ec9e192c76936f0dd821ec1569cb
Tidaratkommong <kommong24@gmail.com>
2020-11-25 20:11:01 +0700

    home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -42,70 +42,111 @@
 
     <br />
     <!--product -->
-    <div class="breadcrumb"><h5><a href="{{url('/homeproduct')}}">ดูสินค้าทั้งหมด</a></h5>
+    <div class="breadcrumb">
+        <h5><a href="{{url('/homeproduct')}}">ดูสินค้าทั้งหมด</a></h5>
         <main>
             <div class="page-overlay-bg">
                 <div class="container-fluid m-0 p-0">
                     <!--container-fluid-->
-                    
 
-                <!--container-fluid-->
 
-            </div><!-- page-main-->
+                    <!--container-fluid-->
+
+                </div><!-- page-main-->
         </main>
     </div>
 
-    ตกแต่งเพิ่มเติม 
+    ตกแต่งเพิ่มเติม
 
-   
+    <div id="myCarousel" class="carousel slide" data-ride="carousel">
+        <!-- Indicators -->
+        <ol class="carousel-indicators">
+            <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+            <li data-target="#myCarousel" data-slide-to="1"></li>
+            <li data-target="#myCarousel" data-slide-to="2"></li>
+        </ol>
+
+        <!-- Wrapper for slides -->
+        <div class="carousel-inner">
+            <div class="item active">
+                <img src="la.jpg" alt="Los Angeles">
+            </div>
+
+            <div class="item">
+                <img src="chicago.jpg" alt="Chicago">
+            </div>
+
+            <div class="item">
+                <img src="ny.jpg" alt="New York">
+            </div>
+        </div>
+
+        <!-- Left and right controls -->
+        <a class="left carousel-control" href="#myCarousel" data-slide="prev">
+            <span class="glyphicon glyphicon-chevron-left"></span>
+            <span class="sr-only">Previous</span>
+        </a>
+        <a class="right carousel-control" href="#myCarousel" data-slide="next">
+            <span class="glyphicon glyphicon-chevron-right"></span>
+            <span class="sr-only">Next</span>
+        </a>
+    </div>
+
+</div>
+
+
+
+
 
 <!-- chatbot-->
 
 <!--<link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">-->
 
-<link href="https://fonts.googleapis.com/css?family=Montserrat:400,500,700,800" rel="stylesheet">      
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: sans-serif;
-                font-weight: 200;
-                height: 100vh;
-                margin: 0;
-            }
-        </style>
-    </head>
-    <body>
-    </body>
-  
-    <link rel="stylesheet" type="text/css" >
-    <script>
-	    var botmanWidget = {
-	        aboutText: 'ssdsd',
-            title: 'Over You 99',
-            mainColor: '#F28240',
-            bubbleBackground: '#F28240',
-            introMessage: '✋สวัสดีค่ะ นี่เป็นข้อความอัตโนมัติ มีอะไรให้เราช่วยไหมคะ ถ้าต้องการเริ่มต้นใหม่ พิมพ์คำว่า "เริ่มใหม่" ได้ตลอดเวลา ',
-            placeholderText: 'Ask Me Something',
-            aboutLink: 'mhdevelopment.gr'
-            
-	    };
-    </script> 
-        <script src='https://cdn.jsdelivr.net/npm/botman-web-widget@0/build/js/widget.js'></script>
+<link href="https://fonts.googleapis.com/css?family=Montserrat:400,500,700,800" rel="stylesheet">
+<style>
+    html,
+    body {
+        background-color: #fff;
+        color: #636b6f;
+        font-family: sans-serif;
+        font-weight: 200;
+        height: 100vh;
+        margin: 0;
+    }
+</style>
+</head>
+
+<body>
+</body>
+
+<link rel="stylesheet" type="text/css">
+<script>
+    var botmanWidget = {
+        aboutText: 'ssdsd',
+        title: 'Over You 99',
+        mainColor: '#F28240',
+        bubbleBackground: '#F28240',
+        introMessage: '✋สวัสดีค่ะ นี่เป็นข้อความอัตโนมัติ มีอะไรให้เราช่วยไหมคะ ถ้าต้องการเริ่มต้นใหม่ พิมพ์คำว่า "เริ่มใหม่" ได้ตลอดเวลา ',
+        placeholderText: 'Ask Me Something',
+        aboutLink: 'mhdevelopment.gr'
+
+    };
+</script>
+<script src='https://cdn.jsdelivr.net/npm/botman-web-widget@0/build/js/widget.js'></script>
 
 
-  <!-- endchatbot --> 
+<!-- endchatbot -->
 
-  
-<br/>
-<br/>
+
+<br />
+<br />
 
 <ul class="nav justify-content-center bg-dark fixed-bottom ">
     <li class="nav-item">
         <a class="btn btn-link" href="#"></a>
     </li>
-    <br/>
-    
+    <br />
+
 </ul>
 
 
