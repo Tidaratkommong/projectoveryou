bc7a4ca87a8033e2d6c0114346f4d4fcc2335315
Tidaratkommong <kommong24@gmail.com>
2020-11-29 19:49:26 +0700

    shop

--- resources/views/layouts/test.blade.php
+++ resources/views/layouts/test.blade.php
@@ -97,7 +97,7 @@
                                     <i class="fas fa-cart-arrow-down text-success fa-2x"></i>
                                     <div class="badge badge-danger">
                                         @auth
-                                        
+                                       <!-- {{Cart::session(auth()->id())->getContent()->count()}}-->
                                         @else
                                         0
                                         @endauth
