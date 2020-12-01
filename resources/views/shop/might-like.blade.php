e684a596cb993646bcc82a6ac19d458e7c03ffde
Tidaratkommong <kommong24@gmail.com>
2020-12-01 17:37:18 +0700

    show

--- resources/views/shop/might-like.blade.php
+++ resources/views/shop/might-like.blade.php
@@ -2,13 +2,8 @@
     <div class="container">
         <h2>You might also like...</h2>
         <div class="might-like-grid">
-            @foreach ($mightAlsoLike as $product)
-                <a href="{{ route('shop.show',$product->id )}}" class="might-like-product">
-                    <img src="#" alt="product">
-                    <div class="might-like-product-name">{{ $product->product_name }}</div>
-                    <div class="might-like-product-price">{{ $product->product_price}}</div>
-                </a>
-            @endforeach
+          
+                
 
         </div>
     </div>
