3eac6793b173b48a26a6b40fd12e0829f6040386
Tidaratkommong <kommong24@gmail.com>
2020-12-01 19:12:40 +0700

    show product

--- resources/views/shop/might-like.blade.php
+++ resources/views/shop/might-like.blade.php
@@ -2,30 +2,30 @@
     <div class="container">
         <h3>คุณอาจจะชอบ...</h3>
         <div class="breadcrumb">
-        <main>
-            <div class="page-overlay-bg">
-                <div class="container-fluid m-0 p-0">
-                    <!--container-fluid-->
-                    <div class="row no-gutters px-0">
-                        <!--row-->
-                        @foreach ($mightAlsoLike as $product)
-                        <div class="col-6 col-sm-4 col-md-3 bg-light px-1">
-                            <div href="javascript:void(0);">
-                                <a  href="{{ route('shop.show',$product->id )}}" class="card  mb-2 shadow-sm" style="width: 16rem; height:20rem;">
-                                    <img class="text-center w-100" src="#" style="width: 150px; height:250px;" />
-                                    <div class=" text-dark text-center"><h5>{{ $product->product_name }}</h5></div>
-                                    <div class="price text-dark text-center"><h5> ราคา :{{ $product->product_name }}<span> บาท  </h5> </span></div>
-                                </a>
+            <main>
+                <div class="page-overlay-bg">
+                    <div class="container-fluid m-0 p-0">
+                        <!--container-fluid-->
+                        <div class="row no-gutters px-0">
+                            <!--row-->
+                            @foreach ($mightAlsoLike as $product)
+                            <div class="col-6 col-sm-4 col-md-3 bg-light px-1">
+                                <div href="javascript:void(0);">
+                                    <a href="{{ route('shop.show',$product->id )}}" class="card  mb-2 shadow-sm" style="width: 16rem; height:20rem;">
+                                        <img class="text-center w-100" src="{{asset($product->product_img )}}" style="width: 150px; height:250px;" />
+                                        <div class="might-like-product-name">{{ $product->product_name }}</div>
+                                        <div class="might-like-product-price">{{ $product->product_price}}</div>
+                                    </a>
+                                </div>
                             </div>
+                            @endforeach
                         </div>
-                        @endforeach
+                        <!--row-->
                     </div>
-                    <!--row-->
-                </div>
-                <!--container-fluid-->
-            </div><!-- page-main-->
-        </main>
-    </div>
+                    <!--container-fluid-->
+                </div><!-- page-main-->
+            </main>
+        </div>
 
 
 
@@ -39,23 +39,23 @@
 
 
 
-            <div class="might-like-grid">
-                @foreach ($mightAlsoLike as $product)
-                <a href="{{ route('shop.show',$product->id )}}" class="might-like-product">
-                    <img src="#" alt="product">
-                    <div class="might-like-product-name">{{ $product->product_name }}</div>
-                    <div class="might-like-product-price">{{ $product->product_name }}</div>
-                </a>
-                @endforeach
-
-            </div>
-
-
-
-
-
+        <div class="might-like-grid">
+            @foreach ($mightAlsoLike as $product)
+            <a href="{{ route('shop.show',$product->id )}}" class="might-like-product">
+                <img src="#" alt="product">
+                <div class="might-like-product-name">{{ $product->product_name }}</div>
+                <div class="might-like-product-price">{{ $product->product_name }}</div>
+            </a>
+            @endforeach
 
         </div>
+
+
+
+
+
+
     </div>
+</div>
 </div>
 © 202
