830b1865b0e1088464e65296fd199e1a49ca2b5a
Tidaratkommong <kommong24@gmail.com>
2020-12-01 19:22:21 +0700

    show_product

--- resources/views/shop/might-like.blade.php
+++ resources/views/shop/might-like.blade.php
@@ -12,7 +12,7 @@
                             <div class="col-6 col-sm-4 col-md-3 bg-light px-1">
                                 <div href="javascript:void(0);">
                                     <a href="{{ route('shop.show',$product->id )}}" class="card  mb-2 shadow-sm" style="width: 16rem; height:20rem;">
-                                        <img class="text-center w-100" src="{{asset($product->product_img )}}" style="max-width: 540px;" >
+                                        <img class="text-center w-100" src="{{asset($product->product_img )}}" style="width: 130px; height:200px;"/ >
                                         <div class="might-like-product-name">{{ $product->product_name }}</div>
                                         <div class="might-like-product-price">{{ $product->product_price}}</div>
                                     </a>
