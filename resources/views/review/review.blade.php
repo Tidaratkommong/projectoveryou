41f4bca43ce81c7a2e45b4b361bb7b52144daaa3
Tidaratkommong <kommong24@gmail.com>
2020-12-06 01:57:10 +0700

    test

--- resources/views/review/review.blade.php
+++ resources/views/review/review.blade.php
@@ -1,11 +1,12 @@
 <div class="container">
-    <div class="breadcrumb">
+    <div class="page-overlay-bg bg-light ">
+        
             <div class="vc_column-inner">
                 <div class="wpb_wrapper">
                     <div class="wpb_text_column wpb_content_element ">
                         <div class="wpb_wrapper">
                             <br>
-                            <h6><span style="font-size: 21px;">&nbsp;&nbsp;&nbsp;&nbsp; รีวิวจากผู้ใช้ </span></h6>
+                            <h6><span style="font-size: 21px;">&nbsp;&nbsp; รีวิวจากผู้ใช้ </span></h6>
                             <hr>
                         </div>
                     </div>
@@ -77,7 +78,123 @@
                     </div>
                 </div>
             </div>
+        </div>
     </div>
 </div>
 
 
+
+
+<div class="vc_column-inner">
+    <div class="wpb_wrapper">
+        <div class="wpb_text_column wpb_content_element ">
+            <div class="wpb_wrapper">
+                <h3><span style="font-size: 21px;">USER REVIEWS</span></h3>
+
+            </div>
+        </div>
+        <div class="vcex-module vcex-divider vcex-divider-solid wpex-max-w-100 wpex-mx-auto wpex-block wpex-h-0 wpex-border-b wpex-border-solid wpex-border-gray-200" style="margin-top:10px;margin-bottom:15px;"></div>
+        <div class="someka-product-page-reviews">
+            <div id="reviews" class="woocommerce-Reviews">
+                <div id="comments">
+                    <h2 class="woocommerce-Reviews-title">
+                        2 reviews for <span>My Reviews: Personal Reviews and Ratings Database</span> </h2>
+
+                    <ol class="commentlist">
+                        <li class="review even thread-even depth-1" id="li-comment-239660">
+
+                            <div id="comment-239660" class="comment_container">
+
+
+                                <div class="comment-text">
+
+
+                                    <p class="meta">
+                                        <strong class="woocommerce-review__author">Giuseppe A. </strong>
+                                        <span class="woocommerce-review__dash">–</span> <time class="woocommerce-review__published-date" datetime="2019-07-16T14:38:13+03:00">July 16, 2019</time>
+                                    </p>
+
+                                    <div class="star-rating" role="img" aria-label="Rated 4 out of 5"><span style="width:80%">Rated <strong class="rating">4</strong> out of 5</span></div>
+                                    <div class="description">
+                                        <p>Overall is great but I just need one more column for additional information</p>
+                                    </div>
+                                </div>
+                            </div>
+                            <ul class="children">
+                                <li class="comment byuser comment-author-ezgi odd alt depth-2" id="li-comment-239662">
+
+                                    <div id="comment-239662" class="comment_container">
+
+
+                                        <div class="comment-text">
+
+
+                                            <p class="meta">
+                                                <strong class="woocommerce-review__author">Someka </strong>
+                                                <span class="woocommerce-review__dash">–</span> <time class="woocommerce-review__published-date" datetime="2019-07-17T14:41:17+03:00">July 17, 2019</time>
+                                            </p>
+
+                                            <div class="description">
+                                                <p>Hello Giuseppe! Thank you for your feedback. You can add more columns, but before that please don’t forget to display the headings of your sheet. You can check this page to learn how to do that: <a href="https://www.someka.net/help/column-headings/">Display Column Headings</a>. After that, just right click to row/column and choose insert option. If you can not handle it, please feel free to contact us!</p>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </li><!-- #comment-## -->
+                            </ul><!-- .children -->
+                        </li><!-- #comment-## -->
+                        <li class="review even thread-odd thread-alt depth-1" id="li-comment-32503">
+
+                            <div id="comment-32503" class="comment_container">
+
+
+                                <div class="comment-text">
+
+
+                                    <p class="meta">
+                                        <strong class="woocommerce-review__author">Julie Kosinda </strong>
+                                        <span class="woocommerce-review__dash">–</span> <time class="woocommerce-review__published-date" datetime="2017-08-23T22:00:25+03:00">August 23, 2017</time>
+                                    </p>
+
+                                    <div class="star-rating" role="img" aria-label="Rated 5 out of 5"><span style="width:100%">Rated <strong class="rating">5</strong> out of 5</span></div>
+                                    <div class="description">
+                                        <p>I needed a system to keep track of reviews, and there just wasn’t an app that worked. This sheet is very easy to use and the owner was very responsive when I had a question. This is the perfect sheet to log reviews!</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </li><!-- #comment-## -->
+                    </ol>
+
+                </div>
+
+                <div id="review_form_wrapper">
+                    <div id="review_form">
+                        <div id="respond" class="comment-respond">
+                            <span id="reply-title" class="comment-reply-title">Leave a customer review <small><a rel="nofollow" id="cancel-comment-reply-link" href="/excel-template/personal-reviews-ratings-database/#respond" style="display:none;">Cancel reply</a></small></span>
+                            <form action="https://www.someka.net/wp-comments-post.php" method="post" id="commentform" class="comment-form" novalidate="">
+                                <p class="comment-notes"><span id="email-notes">Your email address will not be published.</span> Required fields are marked <span class="required">*</span></p>
+                                <div class="comment-form-rating"><label for="rating">Your rating&nbsp;<span class="required">*</span></label>
+                                    <p class="stars"> <span> <a class="star-1" href="#">1</a> <a class="star-2" href="#">2</a> <a class="star-3" href="#">3</a> <a class="star-4" href="#">4</a> <a class="star-5" href="#">5</a> </span> </p><select name="rating" id="rating" required="" style="display: none;">
+                                        <option value="">Rate…</option>
+                                        <option value="5">Perfect</option>
+                                        <option value="4">Good</option>
+                                        <option value="3">Average</option>
+                                        <option value="2">Not that bad</option>
+                                        <option value="1">Very poor</option>
+                                    </select>
+                                </div>
+                                <p class="comment-form-comment"><label for="comment">Your review&nbsp;<span class="required">*</span></label><textarea id="comment" name="comment" cols="45" rows="8" required=""></textarea></p>
+                                <p class="comment-form-author"><label for="author">Name&nbsp;<span class="required">*</span></label><input id="author" name="author" type="text" value="" size="30" required=""></p>
+                                <p class="comment-form-email"><label for="email">Email&nbsp;<span class="required">*</span></label><input id="email" name="email" type="email" value="" size="30" required=""></p>
+                                <p class="form-submit"><input name="submit" type="submit" id="submit" class="submit" value="Submit"> <input type="hidden" name="comment_post_ID" value="305" id="comment_post_ID">
+                                    <input type="hidden" name="comment_parent" id="comment_parent" value="0">
+                                </p>
+                            </form>
+                        </div><!-- #respond -->
+                    </div>
+                </div>
+
+                <div class="clear"></div>
+            </div>
+        </div>
+    </div>
+</div>
