f135606e6861e6d488a96a519a3938d7fdd57d9d
Tidaratkommong <kommong24@gmail.com>
2020-12-06 02:32:55 +0700

    test

--- resources/views/review/review.blade.php
+++ resources/views/review/review.blade.php
@@ -2,21 +2,13 @@
     <div class="container">
         <div class="product-description-review">
             <div class="description-review-title nav" role=tablist>
-                <h6>
-                    <span style="font-size: 21px;">
-                        <a class="active" href="#pro-dec" data-toggle="tab" role="tab" aria-selected="true">
-                            Description
-                        </a>
-                    </span>
-                </h6>
-                <h6>
-                    <span style="font-size: 21px;">
-                        <a href="#pro-review" data-toggle="tab" role="tab" aria-selected="false">
-                         รีวิวจากผู้ใช้
-                        </a>
-                    </span>
-                </h6>
-
+                
+                <a class="active" href="#pro-dec" data-toggle="tab" role="tab" aria-selected="true">
+                    Description
+                </a>
+                <a href="#pro-review" data-toggle="tab" role="tab" aria-selected="false">
+                    Reviews 
+                </a>
             </div>
             <div class="description-review-text tab-content">
                 <div class="tab-pane active show fade" id="pro-dec" role="tabpanel">
@@ -26,109 +18,107 @@
                 </div>
                 <!-- review -->
                 <div class="tab-pane fade" id="pro-review" role="tabpanel">
-                    <div class="breadcrumb">
-                        <div class="page-overlay-bg bg-light">
-                            <div class="vc_column-inner">
-                                <div class="wpb_wrapper">
-                                    <div class="wpb_text_column wpb_content_element ">
-                                        <div class="wpb_wrapper">
-                                            <br>
-                                            <h6><span style="font-size: 21px;">&nbsp;&nbsp; รีวิวจากผู้ใช้ </span></h6>
-                                            <hr>
+                        <div class="breadcrumb">
+                            <div class="page-overlay-bg bg-light">
+                                <div class="vc_column-inner">
+                                    <div class="wpb_wrapper">
+                                        <div class="wpb_text_column wpb_content_element ">
+                                            <div class="wpb_wrapper">
+                                                <br>
+                                                <h6><span style="font-size: 21px;">&nbsp;&nbsp; รีวิวจากผู้ใช้ </span></h6>
+                                                <hr>
+                                            </div>
                                         </div>
-                                    </div>
-                                    <div class="vcex-module vcex-divider vcex-divider-solid wpex-max-w-100 wpex-mx-auto wpex-block wpex-h-0 wpex-border-b wpex-border-solid wpex-border-gray-200" style="margin-top:10px;margin-bottom:15px;"></div>
-                                    <div class="someka-product-page-reviews">
-                                        <div id="reviews" class="woocommerce-Reviews">
-                                            <div id="comments">
-                                                <ol class="commentlist">
-                                                    <div id="comment-32503" class="comment_container">
+                                        <div class="vcex-module vcex-divider vcex-divider-solid wpex-max-w-100 wpex-mx-auto wpex-block wpex-h-0 wpex-border-b wpex-border-solid wpex-border-gray-200" style="margin-top:10px;margin-bottom:15px;"></div>
+                                        <div class="someka-product-page-reviews">
+                                            <div id="reviews" class="woocommerce-Reviews">
+                                                <div id="comments">
+                                                    <ol class="commentlist">
+                                                        <div id="comment-32503" class="comment_container">
 
 
-                                                        <div class="comment-text">
+                                                            <div class="comment-text">
 
 
-                                                            <p class="meta">
-                                                                <strong class="woocommerce-review__author">Julie Kosinda </strong>
-                                                                <span class="woocommerce-review__dash">–</span> <time class="woocommerce-review__published-date" datetime="2017-08-23T22:00:25+03:00">August 23, 2017</time>
-                                                            </p>
+                                                                <p class="meta">
+                                                                    <strong class="woocommerce-review__author">Julie Kosinda </strong>
+                                                                    <span class="woocommerce-review__dash">–</span> <time class="woocommerce-review__published-date" datetime="2017-08-23T22:00:25+03:00">August 23, 2017</time>
+                                                                </p>
 
-                                                            <!--<div class="star-rating" role="img" aria-label="Rated 5 out of 5"><span style="width:100%">Rated <strong class="rating">5</strong> out of 5</span></div> -->
+                                                                <!--<div class="star-rating" role="img" aria-label="Rated 5 out of 5"><span style="width:100%">Rated <strong class="rating">5</strong> out of 5</span></div> -->
 
-                                                            <div class="description">
-                                                                <p>I needed a system to keep track of reviews, and there just wasn’t an app that worked. This sheet is very easy to use and the owner was very responsive when I had a question. This is the perfect sheet to log reviews!</p>
+                                                                <div class="description">
+                                                                    <p>I needed a system to keep track of reviews, and there just wasn’t an app that worked. This sheet is very easy to use and the owner was very responsive when I had a question. This is the perfect sheet to log reviews!</p>
+                                                                </div>
+                                                            </div>
+
+                                                            <div class="comment-text">
+
+
+                                                                <p class="meta">
+                                                                    <strong class="woocommerce-review__author">Julie Kosinda </strong>
+                                                                    <span class="woocommerce-review__dash">–</span> <time class="woocommerce-review__published-date" datetime="2017-08-23T22:00:25+03:00">August 23, 2017</time>
+                                                                </p>
+
+                                                                <!--<div class="star-rating" role="img" aria-label="Rated 5 out of 5"><span style="width:100%">Rated <strong class="rating">5</strong> out of 5</span></div> -->
+
+                                                                <div class="description">
+                                                                    <p>I needed a system to keep track of reviews, and there just wasn’t an app that worked. This sheet is very easy to use and the owner was very responsive when I had a question. This is the perfect sheet to log reviews!</p>
+                                                                </div>
                                                             </div>
                                                         </div>
 
-                                                        <div class="comment-text">
+                                                    </ol>
 
-
-                                                            <p class="meta">
-                                                                <strong class="woocommerce-review__author">Julie Kosinda </strong>
-                                                                <span class="woocommerce-review__dash">–</span> <time class="woocommerce-review__published-date" datetime="2017-08-23T22:00:25+03:00">August 23, 2017</time>
-                                                            </p>
-
-                                                            <!--<div class="star-rating" role="img" aria-label="Rated 5 out of 5"><span style="width:100%">Rated <strong class="rating">5</strong> out of 5</span></div> -->
-
-                                                            <div class="description">
-                                                                <p>I needed a system to keep track of reviews, and there just wasn’t an app that worked. This sheet is
-                                                                     very easy to use and the owner was very responsive when I had a question. This is the perfect sheet to
-                                                                      log reviews!</p>
-                                                            </div>
-                                                        </div>
-                                                    </div>
-
-                                                </ol>
-
-                                            </div>
-
-
-                                            <hr>
-                                            <div id="review_form_wrapper">
-                                                <div id="review_form">
-                                                    <div id="respond" class="comment-respond">
-                                                        <span id="reply-title" class="comment-reply-title"> &nbsp;&nbsp;แสดงความคิดเห็นจากลูกค้า <small><a rel="nofollow" id="cancel-comment-reply-link" href="/excel-template/personal-reviews-ratings-database/#respond" style="display:none;">Cancel reply</a></small></span>
-                                                        <form action="https://www.someka.net/wp-comments-post.php" method="post" id="commentform" class="comment-form" novalidate="">
-                                                            <p class="comment-notes"><span id="email-notes"> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;ที่อยู่อีเมลของคุณจะไม่ถูกเผยแพร่ </span> <span class="required text-danger">*</span></p>
-
-
-                                                            <div class="form-group">
-                                                                <label for="text_review">&nbsp;รีวิวสินค้า <span class="required text-danger">*</span></label>
-                                                                <textarea class="form-control" name="text_review" rows="4" /></textarea>
-                                                            </div>
-
-                                                            <div class="form-row">
-                                                                <div class="form-group col-md-6">
-                                                                    <label for="review_name">&nbsp;ชื่อ<span class="required text-danger">*</span></label>
-                                                                    <input type="text" name="review_name" class="form-control input-lg" value="" />
-                                                                </div>
-
-                                                                <div class="form-group col-md-6">
-                                                                    <label for="review_email">&nbsp;อีเมล <span class="required text-danger">*</span></label>
-                                                                    <input type="text" name="review_email" class="form-control input-lg" value="" />
-                                                                </div>
-
-                                                            </div>
-
-                                                            &nbsp;&nbsp;&nbsp;&nbsp;<button type="button" class="btn btn-primary"> บันทึก </button>
-                                                            <br>
-                                                            <br>
-                                                        </form>
-                                                    </div><!-- #respond -->
                                                 </div>
-                                            </div>
 
-                                            <div class="clear"></div>
+
+                                                <hr>
+                                                <div id="review_form_wrapper">
+                                                    <div id="review_form">
+                                                        <div id="respond" class="comment-respond">
+                                                            <span id="reply-title" class="comment-reply-title"> &nbsp;&nbsp;แสดงความคิดเห็นจากลูกค้า <small><a rel="nofollow" id="cancel-comment-reply-link" href="/excel-template/personal-reviews-ratings-database/#respond" style="display:none;">Cancel reply</a></small></span>
+                                                            <form action="https://www.someka.net/wp-comments-post.php" method="post" id="commentform" class="comment-form" novalidate="">
+                                                                <p class="comment-notes"><span id="email-notes"> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;ที่อยู่อีเมลของคุณจะไม่ถูกเผยแพร่ </span> <span class="required text-danger">*</span></p>
+
+
+                                                                <div class="form-group">
+                                                                    <label for="text_review">&nbsp;รีวิวสินค้า <span class="required text-danger">*</span></label>
+                                                                    <textarea class="form-control" name="text_review" rows="4" /></textarea>
+                                                                </div>
+
+                                                                <div class="form-row">
+                                                                    <div class="form-group col-md-6">
+                                                                        <label for="review_name">&nbsp;ชื่อ<span class="required text-danger">*</span></label>
+                                                                        <input type="text" name="review_name" class="form-control input-lg" value="" />
+                                                                    </div>
+
+                                                                    <div class="form-group col-md-6">
+                                                                        <label for="review_email">&nbsp;อีเมล <span class="required text-danger">*</span></label>
+                                                                        <input type="text" name="review_email" class="form-control input-lg" value="" />
+                                                                    </div>
+
+                                                                </div>
+
+                                                                &nbsp;&nbsp;&nbsp;&nbsp;<button type="button" class="btn btn-primary"> บันทึก </button>
+                                                                <br>
+                                                                <br>
+                                                            </form>
+                                                        </div><!-- #respond -->
+                                                    </div>
+                                                </div>
+
+                                                <div class="clear"></div>
+                                            </div>
                                         </div>
                                     </div>
                                 </div>
                             </div>
                         </div>
                     </div>
-                </div>
 
+                </div>
             </div>
         </div>
     </div>
-</div>
 </div>
