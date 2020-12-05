7b031a79e0ecb202af91d4c7cc5d8999ca501a12
Tidaratkommong <kommong24@gmail.com>
2020-12-06 00:57:29 +0700

    reviews

--- resources/views/review/review.blade.php
+++ resources/views/review/review.blade.php
@@ -1,14 +1,18 @@
 <div class="container">
     <div class="breadcrumb">
-    <br>
+        <br>
+        <br>
         <h5> รีวิวจากผู้ใช้ </h5>
-</hr>
+        <hr>
         <main>
-            <div class="page-overlay-bg">
-                <div class="container-fluid m-0 p-0">
-
-
+            <hr>
+            <div class="media">
+                <img src="..." class="mr-3" alt="...">
+                <div class="media-body">
+                    <h5 class="mt-0">Media heading</h5>
+                    Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.
                 </div>
+            </div>
         </main>
     </div>
 
